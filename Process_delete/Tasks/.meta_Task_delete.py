d466bfca321e47b03674bb161df64fd064485831
Antoine Brun <antoine.brun@openmsa.co>
2022-11-03 11:56:01 +0100

    add a delete process (#1)
    Co-authored-by: Naveen <nku@ubiqube.com>

--- Process_delete/Tasks/.meta_Task_delete.py
+++ Process_delete/Tasks/.meta_Task_delete.py
@@ -3,11 +3,11 @@
     <map>
         <entry>
             <key>DISPLAYNAME</key>
-            <value>my_workflow.xml</value>
+            <value>Task_delete.py</value>
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1667472491673</value>
+            <value>1667472488737</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,11 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1667472491659</value>
-        </entry>
-        <entry>
-            <key>MODEL</key>
-            <value>0</value>
+            <value>1667472488723</value>
         </entry>
         <entry>
             <key>TAG</key>
@@ -29,12 +25,8 @@
             <value>text</value>
         </entry>
         <entry>
-            <key>MANUFACTURER</key>
-            <value>0</value>
-        </entry>
-        <entry>
             <key>TYPE</key>
-            <value>FILE</value>
+            <value>UPLOAD</value>
         </entry>
         <entry>
             <key>COMMENT</key>
